f2985872a558f263faaed3ad192c4aee3d9710cf
Brian Chen <bchen.chen93@gmail.com>
2015-07-30 09:57:23 +1200

    ran test to check if all urls have been defined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,11 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('all have a URL defined', function() {
+          for (i = 0, len = allFeeds.length; i < len; i++) {
+            expect(allFeeds[i].url).toBeDefined();
+          }
+        });
 
 
         /* TODO: Write a test that loops through each feed
